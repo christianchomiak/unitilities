500b8ca0a0a0f6a3b9c60d31089a1bb7b75d6f6b
Christian Chomiak <christianchomiak@gmail.com>
2013-10-22 23:30:38 -0430

    Invisible Object Drawer
    Script to show the position of an object inside the Unity Editor

---
+++ Unity Utilities/Assets/Scripts/EditorHelpers/InvisibleObjectDrawer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class InvisibleObjectDrawer : MonoBehaviour
+{
+    [SerializeField]
+    float radius = 1f;
+    
+    [SerializeField]
+    Color color = Color.white;
+
+    void Awake()
+    {
+        if (radius <= 0f)
+            radius = 1f;
+    }
+
+    /// <summary>
+    /// Draw a sphere to signal the object's position on editor view
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        Gizmos.color = this.color;
+        Gizmos.DrawWireSphere(this.transform.position, this.radius);
+    }
+}
