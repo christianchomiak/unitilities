e989208c92680fb54c2817692553caf7c1ac78bd
Christian Chomiak <christianchomiak@gmail.com>
2014-04-29 23:38:50 -0430

    TimeScaleLayers v1.0
    TimeScale system where each Time Layer can have a custom time scale
    value.
    
    TO-DO:
    * Make it independent from Time.deltaTime
    * Create a better editor (Dictionaries?)

---
+++ Unity Utilities/Assets/Unitilities/Scripts/Work In Progress/TimeScaleLayers/Editor/TimeManagerEditor.cs
@@ -0,0 +1,69 @@
+﻿// MyScriptEditor.cs
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TimeManager))]
+public class TimeManagerEditor : Editor
+{
+    public void OnEnable()
+    {
+        //TimeManager myTarget = (TimeManager)target;     
+
+        //if (myTarget.)
+    }
+
+    public override void OnInspectorGUI()
+    {
+        TimeManager myTarget = (TimeManager) target;
+
+        int prevNumber = myTarget.numberOfLayers;
+
+        myTarget.numberOfLayers = EditorGUILayout.IntField("Number of layers", myTarget.numberOfLayers);
+        myTarget.numberOfLayers = (myTarget.numberOfLayers < 0 ? 0 : myTarget.numberOfLayers);
+
+        if (myTarget.numberOfLayers == 0)
+        {
+            myTarget.layers.Clear();
+            myTarget.scales.Clear();
+            return;
+        }
+
+        int diff = myTarget.numberOfLayers - prevNumber;
+
+
+        if (diff < 0) 
+        {
+            diff *= -1;
+
+            if (diff > myTarget.layers.Count)
+            {
+                myTarget.layers.Clear();
+                myTarget.scales.Clear();
+            }
+            else
+            {
+                myTarget.layers.RemoveRange(myTarget.layers.Count - 1 - diff, diff);
+                myTarget.scales.RemoveRange(myTarget.scales.Count - 1 - diff, diff);
+            }
+
+        }
+        else if (diff > 0)
+        {
+            for (int i = 0; i < diff; i++)
+            {
+                myTarget.layers.Add(TimeManager.TimeLayer.REALTIME);
+                myTarget.scales.Add(1f);
+            }
+        }
+
+        for (int i = 0; i < myTarget.numberOfLayers; i++)
+        {
+            GUILayout.Space(20);
+            myTarget.layers[i] = (TimeManager.TimeLayer) EditorGUILayout.EnumPopup("Layer", myTarget.layers[i]);
+            myTarget.scales[i] = EditorGUILayout.FloatField("Time Scale", myTarget.scales[i]);
+            myTarget.scales[i] = (myTarget.scales[i] < 0f ? 0f : myTarget.scales[i]);
+        }
+
+                //myTarget.MyValue = EditorGUILayout.IntSlider("Val-you", myTarget.MyValue, 1, 10);
+    }
+}
---
+++ Unity Utilities/Assets/Unitilities/Scripts/Work In Progress/TimeScaleLayers/TimeManager.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+
+public class TimeManager : Singleton<TimeManager> 
+{
+    /// <summary>
+    /// Tuple class of one TimeLayer and a flot
+    /// </summary>
+    /// <typeparam name="First">Time Layer</typeparam>
+    /// <typeparam name="Second">float value</typeparam>
+    [System.Serializable]
+    public class TupleTimeLayerScale : Tuple<TimeLayer, float>
+    {
+        public TupleTimeLayerScale(TimeLayer a, float b)
+            : base(a, b)
+        {
+        }
+    }
+
+    public enum TimeLayer { REALTIME };
+
+    public Dictionary<TimeLayer, float> timeScales = new Dictionary<TimeLayer, float>();
+
+    public List<TimeLayer> layers;
+    public List<float> scales;
+    public int numberOfLayers = 0;
+    
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        timeScales = new Dictionary<TimeLayer, float>();
+
+        for (int i = 0; i < numberOfLayers; i++)
+        {
+            float value = 0f;
+
+            if (timeScales.TryGetValue(layers[i], out value))
+            {
+            }
+            else
+            {
+                timeScales.Add(layers[i], scales[i]);
+            }
+        }
+
+        var keys = new List<TimeLayer>(timeScales.Keys);
+        foreach (TimeLayer key in keys)
+        {
+            Debug.Log("K: " + key.ToString() + ", V: " + timeScales[key]);
+        }
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public float Scale(TimeLayer layer)
+    {
+        // When a program often has to try keys that turn out not to 
+        // be in the dictionary, TryGetValue can be a more efficient  
+        // way to retrieve values.
+        float value = 0f;
+
+        if (timeScales.TryGetValue(layer, out value))
+        {
+            return value;
+        }
+        else
+        {
+            return Time.timeScale;
+        }
+    }
+
+    public float Scale()
+    {
+        return Time.timeScale;
+    }
+
+    public float DeltaTime()
+    {
+        return Time.deltaTime;
+    }
+
+    public float DeltaTime(TimeLayer layer)
+    {
+        float scale = Scale(layer);
+
+        return Time.deltaTime * scale;
+    }
+}
